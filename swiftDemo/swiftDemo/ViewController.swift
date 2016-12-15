dcd2b3732ba61bf89733024f32bd87122d487193
h js <hjs@hjsdeimac.local>
2016-12-15 14:13:41 +0800

    项目初始化

---
+++ swiftDemo/swiftDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swiftDemo
+//
+//  Created by hejunsong on 16/12/15.
+//  Copyright © 2016年 hjs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
